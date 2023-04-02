7320ec7e1c707eaeefc322360cb65e689363abc4
Ryan Stinebaugh <ryanstinebaugh@gmail.com>
2023-04-02 13:14:54 -0400

    Linktree test

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <title>Linktree</title>
-      <meta name="description" content="Next.js + Tailwind CSS" />
+      <meta name="description" content="Ryan Stinebaugh Links" />
       <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </>
