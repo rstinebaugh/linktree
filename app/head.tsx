6d8e44dffbc64c2af1bd10c7a828151c4e8bde8b
Ryan Stinebaugh <ryanstinebaugh@gmail.com>
2023-04-09 00:11:23 -0400

    Title fix

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Linktree</title>
+      <title>Ryan Stinebaugh Links</title>
       <meta name="description" content="Ryan Stinebaugh Links" />
       <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
